ec672cddacaa59849ea272a46efb3bbe27593c99
hswg94 <hswg94@gmail.com>
2024-10-22 22:03:57 +0800

    added mkdir line

--- codedeploy_install.sh
+++ codedeploy_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/ecomm-express-api-main || { echo "Directory not found"; exit 1; }
+mkdir /ecomm-express-api-main
+cd /ecomm-express-api-main || { echo "Directory not found"; exit 1; }
 npm i || { echo "npm install failed"; exit 1; }
--- codedeploy_start.sh
+++ codedeploy_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/ecomm-express-api-main || { echo "Directory not found"; exit 1; }
+cd /ecomm-express-api-main || { echo "Directory not found"; exit 1; }
 npm run server -- --port 5000 --host 0.0.0.0 || { echo "npm run failed"; exit 1; }
