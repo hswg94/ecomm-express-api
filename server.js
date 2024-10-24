b93edff6e3c3af318d6874541c75c3c83e6d4b93
hswg94 <hswg94@gmail.com>
2024-10-24 16:58:23 +0800

    test commit for code deploy

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/health', (req, res) => {
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 app.get('/', (req, res) => {
-  res.send('API is running, test aws pipeline');
+  res.send('API is running, final test');
 });
 
 app.use(notFound);
