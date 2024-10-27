456ddf8821fd590ca011a9d6bda97f6d363b2ffd
hswg94 <hswg94@gmail.com>
2024-10-28 02:26:58 +0800

    changed samesite to none

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "None",
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
