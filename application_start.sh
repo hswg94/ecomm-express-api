38fe03f1d8fd519a200ba539181678d4a04b9132
hswg94 <hswg94@gmail.com>
2025-03-02 22:29:23 +0800

    updated secrets fetch path

--- application_start.sh
+++ application_start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Retrieve secrets from Secrets Manager
-MONGO_URI=$(aws secretsmanager get-secret-value --secret-id MONGO_URI --query SecretString --output text | jq -r .MONGO_URI)
-JWT_SECRET=$(aws secretsmanager get-secret-value --secret-id JWT_SECRET --query SecretString --output text | jq -r .JWT_SECRET)
-PAYPAL_CLIENT_ID=$(aws secretsmanager get-secret-value --secret-id PAYPAL_CLIENT_ID --query SecretString --output text | jq -r .PAYPAL_CLIENT_ID)
-PAYPAL_APP_SECRET=$(aws secretsmanager get-secret-value --secret-id PAYPAL_APP_SECRET --query SecretString --output text | jq -r .PAYPAL_APP_SECRET)
-CLOUDINARY_CLOUD_NAME=$(aws secretsmanager get-secret-value --secret-id CLOUDINARY_CLOUD_NAME --query SecretString --output text | jq -r .CLOUDINARY_CLOUD_NAME)
-CLOUDINARY_API_KEY=$(aws secretsmanager get-secret-value --secret-id CLOUDINARY_API_KEY --query SecretString --output text | jq -r .CLOUDINARY_API_KEY)
-CLOUDINARY_API_SECRET=$(aws secretsmanager get-secret-value --secret-id CLOUDINARY_API_SECRET --query SecretString --output text | jq -r .CLOUDINARY_API_SECRET)
+MONGO_URI=$(aws secretsmanager get-secret-value --secret-id ECOMM --query SecretString --output text | jq -r .MONGO_URI)
+JWT_SECRET=$(aws secretsmanager get-secret-value --secret-id ECOMM --query SecretString --output text | jq -r .JWT_SECRET)
+PAYPAL_CLIENT_ID=$(aws secretsmanager get-secret-value --secret-id ECOMM --query SecretString --output text | jq -r .PAYPAL_CLIENT_ID)
+PAYPAL_APP_SECRET=$(aws secretsmanager get-secret-value --secret-id ECOMM --query SecretString --output text | jq -r .PAYPAL_APP_SECRET)
+CLOUDINARY_CLOUD_NAME=$(aws secretsmanager get-secret-value --secret-id ECOMM --query SecretString --output text | jq -r .CLOUDINARY_CLOUD_NAME)
+CLOUDINARY_API_KEY=$(aws secretsmanager get-secret-value --secret-id ECOMM --query SecretString --output text | jq -r .CLOUDINARY_API_KEY)
+CLOUDINARY_API_SECRET=$(aws secretsmanager get-secret-value --secret-id ECOMM --query SecretString --output text | jq -r .CLOUDINARY_API_SECRET)
 
 # Set environmental variables
 export NODE_ENV=production
