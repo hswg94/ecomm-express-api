27ba1d214b23331912721254a35b6a56e3987dc8
hswg94 <hswg94@gmail.com>
2025-05-25 20:10:53 +0800

    fix: remove unnecessary packages from after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x  #enables debug mode, which causes each command to be printed to the terminal before it is executed.
-yum install -y nodejs unzip wget aws-cli jq
+yum install -y nodejs unzip
 npm install -g pm2
 cd /ecomm-express-api
 npm i
