c210a64b788755e7c7ff4abd09fc22463ddc5884
hswg94 <hswg94@gmail.com>
2025-05-25 16:23:40 +0800

    fix: update secure cookie to prevent CSRF attacks

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV == "production",
     sameSite: "none",
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
